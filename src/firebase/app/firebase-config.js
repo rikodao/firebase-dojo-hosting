652e15c6d27dee3d3e52eeb24252a21ba079343c
rikodao <rikodao@gmail.com>
2019-08-21 13:42:12 +0900

    edit firabase-config.js

--- src/firebase/app/firebase-config.js
+++ src/firebase/app/firebase-config.js
@@ -1,4 +1,4 @@
-export default {
+const firebaseConfig = {
   apiKey: "",
   authDomain: "",
   databaseURL: "",
@@ -7,3 +7,5 @@ export default {
   messagingSenderId: "",
   appId: ""
 };
+
+export default firebaseConfig;
